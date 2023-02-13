f68a2a1b1c6871d42f19ad56dca97cb4aa2d7417
raj <raj@gmail.com>
2023-02-13 16:33:31 +0530

    swap two number

---
+++ src/main/java/hunderedquestion/demo7.java
@@ -0,0 +1,15 @@
+package hunderedquestion;
+
+import java.util.Scanner;
+
+public class demo7 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int num1=12;
+        int num2=11;
+        num1=num1+num2;
+        num2 =num1-num2;
+        num1 =num1-num2;
+        System.out.println("number1 "+num1+ " "+"number2 " +num2);
+    }
+}
