f5b51756539770562c902b3ec4d871e1a82ca530
raj <raj@gmail.com>
2023-02-14 09:37:55 +0530

    using for loop print tringle

---
+++ src/main/java/hunderedquestion/Demo9.java
@@ -0,0 +1,22 @@
+package hunderedquestion;
+
+import java.util.Scanner;
+
+public class Demo9 {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int num = scanner.nextInt();
+        int num=6;
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <i; j++) {
+
+                System.out.print(i);
+
+
+            }
+            System.out.println();
+
+        }
+    }
+}
