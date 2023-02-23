ac0103edac5398efa59e472766a5a8a9fec53ee6
raj <raj@gmail.com>
2023-02-23 09:17:49 +0530

    take two parameter and retrun the value

--- src/main/java/hunderedquestion/Drawable.java
+++ src/main/java/hunderedquestion/Drawable.java
@@ -1,13 +1,13 @@
 package hunderedquestion;
 
 public interface Drawable {
-    public String draw(String name);
+    public String draw(String name,String name1);
 
 }
   class  demo19{
       public static void main(String[] args) {
-          Drawable d =(name)->{return "raj"+name;};
-          System.out.println(d.draw("how are you"));
+          Drawable d =(name,name1)->{return "raj"+name +" "+name1;};
+          System.out.println(d.draw("how are you","you wanna go to outside"));
 
       }
 
