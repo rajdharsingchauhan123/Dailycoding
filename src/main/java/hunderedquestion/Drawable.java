6294ac8abe819103f419b0baff52a1bb61d95460
raj <raj@gmail.com>
2023-02-23 09:15:15 +0530

    using lambda passing parameter

--- src/main/java/hunderedquestion/Drawable.java
+++ src/main/java/hunderedquestion/Drawable.java
@@ -1,13 +1,13 @@
 package hunderedquestion;
 
 public interface Drawable {
-    public String draw();
+    public String draw(String name);
 
 }
   class  demo19{
       public static void main(String[] args) {
-          Drawable d =()->{return "draw somthing";};
-          System.out.println(d.draw());
+          Drawable d =(name)->{return "raj"+name;};
+          System.out.println(d.draw("how are you"));
 
       }
 
