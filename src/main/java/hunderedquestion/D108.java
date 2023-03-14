2443a4133f826dc278cb3595c51943e7a7f2e9a9
raj <raj@gmail.com>
2023-03-14 11:38:50 +0530

    sum of digit multiple by 3

---
+++ src/main/java/hunderedquestion/D108.java
@@ -0,0 +1,12 @@
+package hunderedquestion;
+
+public class D108 {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=0;i<=10;i++){
+             sum =3*sum+i;
+
+        }
+        System.out.println(sum);
+    }
+}
