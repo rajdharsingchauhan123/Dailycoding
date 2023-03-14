fe804f5ef41be37fb5936fa74cc44cf30437b4d3
raj <raj@gmail.com>
2023-03-14 10:37:12 +0530

    sum of digit

---
+++ src/main/java/hunderedquestion/Day105.java
@@ -0,0 +1,17 @@
+package hunderedquestion;
+
+import java.util.Scanner;
+
+public class Day105 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int sum=0;
+
+        for(int i=0;i<5;i++){
+            sum=sum+i;
+
+
+        }
+        System.out.println(sum);
+    }
+}
