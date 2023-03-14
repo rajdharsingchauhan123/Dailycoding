9a73e4bb5a1b5263d353f6efc51e8567daa8056c
raj <raj@gmail.com>
2023-03-14 10:21:36 +0530

    count the number

---
+++ src/main/java/hunderedquestion/Day103.java
@@ -0,0 +1,24 @@
+package hunderedquestion;
+
+import java.util.Scanner;
+
+public class Day103 {
+
+    // print the digit of number 12222
+
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+       int arr[]={1,2,3,5,4};
+       int count=0;
+
+      for (int i=0;i<arr.length;i++){
+          count++;
+      }
+        System.out.println(count);
+
+
+
+
+
+    }
+}
