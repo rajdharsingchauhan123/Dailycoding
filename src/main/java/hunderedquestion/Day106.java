a14964a70ee280003fc13e624c9391056ab91056
raj <raj@gmail.com>
2023-03-14 11:16:16 +0530

    duck number

---
+++ src/main/java/hunderedquestion/Day106.java
@@ -0,0 +1,15 @@
+package hunderedquestion;
+
+public class Day106 {
+    public static void main(String[] args) {
+        // duck number
+
+        int arr[]={1,2,46,0};
+        for(int i=0;i< arr.length;i++){
+          if(arr[i]==0){
+              System.out.println("number is duck");
+          }
+
+        }
+    }
+    }
