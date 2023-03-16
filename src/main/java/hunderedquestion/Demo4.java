44970a8fc9219eba186ba8560c9823ffd0e1ddff
raj <raj@gmail.com>
2023-03-16 09:07:35 +0530

    extract a number of givven number

---
+++ src/main/java/hunderedquestion/Demo4.java
@@ -0,0 +1,10 @@
+package hunderedquestion;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        String number="123345";
+        for(int i=0;i<number.length();i++){
+            System.out.println(i);
+        }
+    }
+}
