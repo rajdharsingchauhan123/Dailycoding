cd3841a6ffdddbe0e43c93ef5e1c3a9ab2842a3b
raj <raj@gmail.com>
2023-03-16 11:01:27 +0530

    sum of all postive and negative value

--- src/main/java/hunderedquestion/Day33.java
+++ src/main/java/hunderedquestion/Day33.java
@@ -2,16 +2,21 @@ package hunderedquestion;
 
 public class Day33 {
     public static void main(String[] args) {
-        int arr[] = {1, -2, 4, 6, -3, -6};
+        int arr[] = {1, -299, 4, 6, -3, -6};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
                 sum = sum + arr[i];
 
             }
+            else if (arr[i]<0){
+                sum=sum+arr[i];
+
+            }
 
 
         }
+//        System.out.println(sum);
         System.out.println(sum);
     }
 }
