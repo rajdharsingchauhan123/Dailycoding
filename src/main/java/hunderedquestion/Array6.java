9337bb57a3b5844945454a92356d461a29299700
raj <raj@gmail.com>
2023-03-16 09:48:12 +0530

    printtable and squre

---
+++ src/main/java/hunderedquestion/Array6.java
@@ -0,0 +1,19 @@
+package hunderedquestion;
+
+import java.util.Scanner;
+
+public class Array6 {
+    public static void main(String[] args) {
+        int table;
+        int t;
+        for(int i=1; i<=10;i++){
+            table=2*i;
+            t=table*table;
+
+
+            System.out.println(t);
+
+
+        }
+
+    }}
