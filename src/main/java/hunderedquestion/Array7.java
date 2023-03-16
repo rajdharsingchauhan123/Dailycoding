de2c4bfb227fd41dbb7d68305f36cad4f88dc0a1
raj <raj@gmail.com>
2023-03-16 09:58:45 +0530

    using buffre reader

--- src/main/java/hunderedquestion/Array7.java
+++ src/main/java/hunderedquestion/Array7.java
@@ -1,9 +1,15 @@
 package hunderedquestion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Array7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        char n;
+//        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+//       n= (char)bufferedReader.read();
         Scanner scanner =new Scanner(System.in);
         char s =scanner.next().charAt(0);
 
