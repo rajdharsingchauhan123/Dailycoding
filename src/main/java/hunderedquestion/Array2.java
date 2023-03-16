877ea0845d047c87d2d17aa4e18dd771bee9d3de
raj <raj@gmail.com>
2023-03-16 08:55:57 +0530

    largest number in among three number

---
+++ src/main/java/hunderedquestion/Array2.java
@@ -0,0 +1,22 @@
+package hunderedquestion;
+
+public class Array2 {
+    // how to find largest number among three number
+    public static void main(String[] args) {
+        int a=12;
+        int b=15;
+        int c=234;
+        if(a>=b && a>c){
+            System.out.println(a+"is the largest number");
+        } else if (b>c && b>a) {
+            System.out.println(b+"is the largest number");
+
+
+
+        }
+        else
+            System.out.println(c+" is the largest number ");
+
+    }
+
+}
