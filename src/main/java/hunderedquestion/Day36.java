51dc1b20db72ea375d717d31f90adf4993d1b4c1
raj <raj@gmail.com>
2023-03-16 11:59:21 +0530

    print odd index element

---
+++ src/main/java/hunderedquestion/Day36.java
@@ -0,0 +1,14 @@
+package hunderedquestion;
+
+public class Day36 {
+    public static void main(String[] args) {
+        int arr[]={1,2,4,5,6,7};
+        for(int i=0;i<arr.length;i++){
+           if(i%2!=0){
+               System.out.println(arr[i]);
+
+
+            }
+        }
+    }
+}
