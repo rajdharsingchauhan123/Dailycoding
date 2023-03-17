380609a406a46c0486d8b2e92a8f6ae4c7d6f951
raj <raj@gmail.com>
2023-03-17 09:29:00 +0530

    second smallest and first smallest

--- src/main/java/hunderedquestion/ArrayDay12.java
+++ src/main/java/hunderedquestion/ArrayDay12.java
@@ -1,9 +1,29 @@
 package hunderedquestion;
 
 public class ArrayDay12 {
+
     public static void main(String[] args) {
-        // find the two largest element in the array
+        // find the second largest & smallest element in the array
         int arr[]={1,2,4,23,5,666};
         int n =arr.length;
+        int temp;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Second Largest:"+arr[n-2]);
+        System.out.println("Smallest:"+arr[0]);
     }
-}
+
+
+    }
+
