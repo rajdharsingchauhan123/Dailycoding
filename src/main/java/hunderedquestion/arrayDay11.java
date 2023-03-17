98db1bc5db2d905cf3d96f0a818c4202fd92a6ac
raj <raj@gmail.com>
2023-03-17 08:25:56 +0530

    find the maximun element in array

---
+++ src/main/java/hunderedquestion/arrayDay11.java
@@ -0,0 +1,13 @@
+package hunderedquestion;
+
+import java.util.Arrays;
+
+public class arrayDay11 {
+    public static void main(String[] args) {
+        int arr[]={1,222,4,5,6};
+        int n =arr.length;
+        Arrays.sort(arr);
+        System.out.println(arr[n-1]);
+
+    }
+}
