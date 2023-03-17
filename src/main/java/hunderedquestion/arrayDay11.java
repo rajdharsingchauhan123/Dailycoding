3ee510f5c60babf822cbea95f2142afb4689d301
raj <raj@gmail.com>
2023-03-17 08:33:45 +0530

    find the max element in the array

--- src/main/java/hunderedquestion/arrayDay11.java
+++ src/main/java/hunderedquestion/arrayDay11.java
@@ -6,8 +6,18 @@ public class arrayDay11 {
     public static void main(String[] args) {
         int arr[]={1,222,4,5,6};
         int n =arr.length;
-        Arrays.sort(arr);
-        System.out.println(arr[n-1]);
+//        Arrays.sort(arr);
+//        System.out.println(arr[n-1]);
+        int max;
+        max= arr[0];
+
+        for(int i=0;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+
+        }
+        System.out.println(max);
 
     }
 }
