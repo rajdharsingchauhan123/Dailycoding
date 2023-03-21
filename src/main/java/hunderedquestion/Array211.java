89b6d20e66c47e009e256f5a7bc45cbc9d78326a
raj <raj@gmail.com>
2023-03-21 13:54:21 +0530

    print second max element in array

--- src/main/java/hunderedquestion/Array211.java
+++ src/main/java/hunderedquestion/Array211.java
@@ -1,10 +1,27 @@
 package hunderedquestion;
 
+import java.util.Arrays;
+
 public class Array211 {
+    public  static  int print2largest(int arr[],int n){
+
+        Arrays.sort(arr);
+        int large=arr[n-1];
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]<large){
+                large=arr[i];
+                break;
+            }
+        }
+//        if(large==arr[n-1]){
+//            return -1;
+//        }
+        return large;
+
+    }
     public static void main(String[] args) {
         int arr[]={16,17,4,3,5,2};
-        System.out.print(arr[1]);
-        System.out.print(arr[4]);
-        System.out.print(arr[arr.length-1]);
+     int n =arr.length;
+        System.out.println(print2largest(arr,n));
     }
 }
