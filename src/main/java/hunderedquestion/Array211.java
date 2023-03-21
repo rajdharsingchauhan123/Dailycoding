9a7a68beaef28662c2692207f1f591cc2c3d91fe
raj <raj@gmail.com>
2023-03-21 12:12:29 +0530

    add the index of elemtnet

---
+++ src/main/java/hunderedquestion/Array211.java
@@ -0,0 +1,10 @@
+package hunderedquestion;
+
+public class Array211 {
+    public static void main(String[] args) {
+        int arr[]={16,17,4,3,5,2};
+        System.out.print(arr[1]);
+        System.out.print(arr[4]);
+        System.out.print(arr[arr.length-1]);
+    }
+}
