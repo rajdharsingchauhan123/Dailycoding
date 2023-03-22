b0794ba33985daedb96c340a8bf5949b52f3f352
raj <raj@gmail.com>
2023-03-22 08:49:54 +0530

    find the sum of n natural number

---
+++ src/main/java/hunderedquestion/Array222.java
@@ -0,0 +1,12 @@
+package hunderedquestion;
+
+public class Array222 {
+    public static int seriesSum(int n){
+        return n*(n+1)/2;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println( seriesSum(n));
+
+    }
+}
