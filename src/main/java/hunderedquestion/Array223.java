df04b7607dacd8bfae671c8430684fcde9c994d2
raj <raj@gmail.com>
2023-03-22 09:02:07 +0530

    print alternate in array

---
+++ src/main/java/hunderedquestion/Array223.java
@@ -0,0 +1,10 @@
+package hunderedquestion;
+
+public class Array223 {
+    public static void main(String[] args) {
+        int arr[]={2,3,4,3,21,2,4};
+        for(int i=1;i<arr.length;i=i+2){
+            System.out.println(arr[i]);
+        }
+    }
+}
