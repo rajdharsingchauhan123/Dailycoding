11c5fa66f4af9b21e462233b0ee1237edfaf54f7
raj <raj@gmail.com>
2023-03-22 09:05:19 +0530

    print alternate in another method

--- src/main/java/hunderedquestion/Array223.java
+++ src/main/java/hunderedquestion/Array223.java
@@ -2,9 +2,11 @@ package hunderedquestion;
 
 public class Array223 {
     public static void main(String[] args) {
-        int arr[]={2,3,4,3,21,2,4};
-        for(int i=1;i<arr.length;i=i+2){
-            System.out.println(arr[i]);
+        int arr[]={2,3,4,3,21,2};
+        for(int i=0;i<arr.length;i=i+2){
+         if(i%2==0){
+             System.out.println(arr[i]+" ");
+         }
         }
     }
 }
