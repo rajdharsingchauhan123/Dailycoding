92a2774002ac6ecf288f32be74051527531736e4
raj <raj@gmail.com>
2023-03-28 17:55:26 +0530

    using throw keyword handling project

--- src/main/java/hunderedquestion/exception/ExceptionDemo11.java
+++ src/main/java/hunderedquestion/exception/ExceptionDemo11.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class ExceptionDemo11 {
     public static void main(String[] args) {
-        Scanner s =new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("please inter a number");
-        int roll=s.nextInt();
-        if(roll<0){
-            throw new ArithmeticException("roll number cant'be negative");
+        int roll = s.nextInt();
+        try {
 
-        }else {
-            System.out.println("valid roll number");
+
+            if (roll < 0) {
+                throw new ArithmeticException("roll number cant'be negative");
+
+            } else {
+                System.out.println("valid roll number");
+            }
+
+        } catch (ArithmeticException e) {
+            System.out.println("an exception thrown");
+            System.out.println(e.getMessage());
         }
     }
 }
