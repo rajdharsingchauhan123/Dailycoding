04bf6270613ac4bb3d93fcadb6ee59ff763776f8
raj <raj@gmail.com>
2023-03-30 10:24:07 +0530

    synchronized block

--- src/main/java/hunderedquestion/exception/multithreading/Power.java
+++ src/main/java/hunderedquestion/exception/multithreading/Power.java
@@ -1,22 +1,26 @@
 package hunderedquestion.exception.multithreading;
 
 public class Power {
-    synchronized void printPower(int n){
+    void printPower(int n) {
 
-        int temp=1;
-        for(int i=1;i<=5;i++){
-            System.out.println(Thread.currentThread().getName() + ":- " +n + "^"+ i + " value: " + n*temp);
-            temp = n*temp;
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){System.out.println(e);}
+        synchronized (this) {
+
+            int temp = 1;
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + ":- " + n + "^" + i + " value: " + n * temp);
+                temp = n * temp;
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+
         }
 
 
     }
-
-
-
 }
 class Thread1 extends Thread{
     Power p;
