731b8227aec9f00072149923ae4b09568de51efd
raj <raj@gmail.com>
2023-03-31 12:25:15 +0530

    if present

--- src/main/java/hunderedquestion/optional/OptionalCountry.java
+++ src/main/java/hunderedquestion/optional/OptionalCountry.java
@@ -5,14 +5,18 @@ import java.util.Optional;
 public class OptionalCountry {
 
     public static void main(String[] args) {
-        String []countries= new String[5];
+        String[] countries = new String[5];
 
-        countries[3]="india";
+        countries[3] = "india";
         Optional<String> checkCountry = Optional.ofNullable(countries[3]);
-        if(checkCountry.isPresent()){
-            String country = countries[3].toUpperCase();
-            System.out.println(country);
-        }
-        System.out.println("country is null ");
+        Optional<String> country = Optional.of("india");
+        country.ifPresent(v -> System.out.println(v.toUpperCase()));
+
+//        if(checkCountry.isPresent()){
+//            String country = countries[3].toUpperCase();
+//            System.out.println(country);
+//        }
+//        System.out.println("country is null ");
+//    }
     }
 }
