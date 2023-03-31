aa9e1c334c9b3934057d36f8ae41fba749afe30f
raj <raj@gmail.com>
2023-03-31 11:56:52 +0530

    cheack country present or not

--- src/main/java/hunderedquestion/optional/OptionalCountry.java
+++ src/main/java/hunderedquestion/optional/OptionalCountry.java
@@ -6,6 +6,8 @@ public class OptionalCountry {
 
     public static void main(String[] args) {
         String []countries= new String[5];
+
+        countries[3]="india";
         Optional<String> checkCountry = Optional.ofNullable(countries[3]);
         if(checkCountry.isPresent()){
             String country = countries[3].toUpperCase();
