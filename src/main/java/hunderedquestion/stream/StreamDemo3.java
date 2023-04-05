3d062309efda3dcfa42db311f2a77af0d4273df0
raj <raj@gmail.com>
2023-04-05 20:08:18 +0530

    using array list perform stream operation

--- src/main/java/hunderedquestion/stream/StreamDemo3.java
+++ src/main/java/hunderedquestion/stream/StreamDemo3.java
@@ -2,15 +2,18 @@ package hunderedquestion.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamDemo3 {
     public static void main(String[] args) {
         Stream<String>strings=Stream.of("one","two","three","four");
-        List<String>result=new ArrayList<>();
+//        List<String>result=new ArrayList<>();
 
-
-      strings.filter(s -> s.length()==3).map(String::toUpperCase).forEach(result::add);
-        System.out.println(result);
+//
+//      strings.filter(s -> s.length()==3).map(String::toUpperCase).forEach(result::add);
+//        System.out.println(result);
+        List<String>result1=strings.filter(s -> s.length()==3).map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(result1);
     }
 }
